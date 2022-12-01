05def9e149130f96920aaecbd445b135b5b3075b
chenhan <chenhan@jixiang.cn>
2022-12-01 16:06:35 +0800

    1.1.2

--- RXWXSDK.framework/Headers/RXWXService.h
+++ RXWXSDK.framework/Headers/RXWXService.h
@@ -24,6 +24,12 @@ NS_ASSUME_NONNULL_BEGIN
 /**
  * 微信分享
  * @param shareInfo 获取分享信息返回的内容  必须
+ * ！shareInfo参数说明：
+ * ！title 链接标题
+ * ！url 分享链接
+ * ！material_type 分享类型
+ * ！image 分享图片
+ * ！content 分享文案
  */
 - (void)shareToWWithShareInfo:(NSDictionary *)shareInfo
                      complete:(void(^)(BOOL success))complete;
@@ -33,13 +39,13 @@ NS_ASSUME_NONNULL_BEGIN
  * @param func 埋点标识  必须
  * @param platform 分享平台 wechat
  * @param region 地区码  非必须
- * @param transmits 透传参数，原样返回  非必须
+ * @param transmits 透传参数，原样返回， 请使用key=value形式，并对值使用urlencode，返回时会原样返回  非必须
  * @param ext 扩展字段，拼接url用  非必须
  */
 - (void)shareToWWithFunc:(NSString *)func
                 platform:(NSString *)platform
                   region:(NSString *)region
-               transmits:(NSDictionary * _Nullable)transmits
+               transmits:(NSString * _Nullable)transmits
                      ext:(NSDictionary * _Nullable)ext
                 complete:(void(^)(BOOL success))complete;
 
@@ -59,7 +65,9 @@ NS_ASSUME_NONNULL_BEGIN
 - (BOOL)isWXAppInstalled;
 
 /**
- * 处理跳转
+ * 处理微信通过Universal Link启动App时传递的数据
+ * 需要在 application:openURL:sourceApplication:annotation:或者application:handleOpenURL中调用。
+ * @param url 微信启动第三方应用时传递过来的URL
  */
 - (BOOL)handleOpenUrl:(NSURL *)url;
 
